a990a16a31bcb1a979c3069f998b8e36946f692e
Elena Romanenko <murenec_elena@mail.ru>
2022-10-31 21:53:09 +0600

    решена задача 25

---
+++ ex25/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 25. Найти сумму чисел от 1 до А
+
+int numberA = new Random().Next(1, 100);
+Console.WriteLine($"Ввод числа {numberA}");
+int sum = 0;
+int digit = 1;
+
+while (digit <= numberA)
+{
+    sum = sum + digit;
+    digit = digit + 1;
+}
+
+Console.WriteLine($"Сумма чисел от 1 до {numberA} равна {sum}");
